a6b8f8b20467b57e45b951c875a849339be848d2
Lê Barros <69652623+FluffyThread@users.noreply.github.com>
2023-04-15 14:24:39 -0300

    feat: mais testes pra coleção

--- tests/clientsBusiness.test.ts
+++ tests/clientsBusiness.test.ts
@@ -1,5 +1,7 @@
 import { ClientsBusiness } from "../src/business/ClientsBusiness";
 import { ClientsDatabase } from "../src/data/ClientsDatabase";
+import { ClientsDTO } from "../src/models/ClientsDTO";
+import { Payment } from "../src/models/PaymentDTO";
 
 
 jest.mock('../src/data/ClientsDatabase', () => ({
@@ -47,4 +49,61 @@ describe("register", () => {
         expect(id.length).toBeGreaterThan(0);
     });
 });
+
+describe("getClientWithPayments", () => {
+    let clientsBusiness: ClientsBusiness;
+    let clientsDatabase: ClientsDatabase;
+    beforeEach(() => {
+        clientsDatabase = {
+            register: jest.fn(),
+        } as unknown as ClientsDatabase;
+
+        clientsBusiness = new ClientsBusiness(clientsDatabase);
+    });
+  
+    test("should throw an error when missing user ID", async () => {
+      expect.assertions(1);
+      try {
+        await clientsBusiness.getClientWithPayments("");
+      } catch (error:any) {
+        expect(error.message).toBe("Missing ID");
+      }
+    });
+  
+    test("should return client with payments successfully", async () => {
+        const mockClient: ClientsDTO = {
+          id: "123",
+          name: "John Doe",
+          email: "johndoe@example.com",
+          cpf: "12345678901",
+        };
+      
+        const mockPayment: Payment = {
+          id: "456",
+          amount: 100,
+          type:"credit_card",
+          card_holder:"John Doe",
+          card_number: "1234 5678 9012 3456",
+          card_expiration_date:"07/2030",
+          card_cvv:"132",
+          client_id: "123",
+        };
+      
+        const getClientWithPaymentsMock = jest.fn().mockImplementation(async (clientId: string) => {
+          return {
+            client: mockClient,
+            payments: [mockPayment],
+          };
+        });
+      
+        clientsDatabase.getClientWithPayments = getClientWithPaymentsMock;
+      
+        const response = await clientsBusiness.getClientWithPayments("123");
+      
+        expect(response.client).toEqual(mockClient);
+        expect(response.payments).toEqual([mockPayment]);
+        expect(clientsDatabase.getClientWithPayments).toHaveBeenCalledWith("123");
+        expect(getClientWithPaymentsMock).toHaveBeenCalledWith("123");
+      });
+  });
 
